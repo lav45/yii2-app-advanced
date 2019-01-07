1449c18dd4af15dd25c5e0a5c2341cbd73000d51
Alex <LAV451@gmail.com>
2019-01-07 16:54:47 +0300

    Update main.php

--- admin/components/gii/views/layouts/main.php
+++ admin/components/gii/views/layouts/main.php
@@ -6,7 +6,7 @@ use yii\helpers\Html;
 /* @var $this \yii\web\View */
 /* @var $content string */
 
-$asset = yii\gii\GiiAsset::register($this);
+$asset = yii\gii\assets\GiiAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
